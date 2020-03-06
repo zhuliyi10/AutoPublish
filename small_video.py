2c32641044754b2878ab89547f7c84743416a7e7
zhuliyi10 <2280835569@qq.com>
2020-03-06 23:26:08 +0800

    小视频封面上传

--- small_video.py
+++ small_video.py
@@ -13,7 +13,7 @@ from qwindow import Qwindow
 
 # 上传今日头条小视频
 def uploadTaoTiao(videoBean):
-    wait = ui.WebDriverWait(browser, 30)  # 10秒内每隔500毫秒扫描1次页面变化
+    wait = ui.WebDriverWait(browser, 100)  # 10秒内每隔500毫秒扫描1次页面变化
     browser.execute_script('window.open()')
     browser.switch_to_window(browser.window_handles[len(browser.window_handles)-1])
     # # 今日头条上传视频
@@ -25,33 +25,41 @@ def uploadTaoTiao(videoBean):
     # 等待页面加载
     print("开始上传今日头条小视频")
     wait.until(lambda driver: browser.find_element_by_xpath("//div[@class='sv-video-uploaded']"))
-    print("今日头条小视频上传成功")
-    browser.find_element_by_xpath("//textarea[@class='tui2-input']").send_keys(
-        videoBean.title)
-    browser.find_element_by_xpath("//button[@class='tui2-btn tui2-btn-size-small tui2-btn-primary publish-btn']").click()    
 
+    # 上传封面
+    browser.find_element_by_xpath("//span[@class='sv-video-change-cover']").click()
+    sleep(2)
+    browser.find_element_by_xpath("//div[@class=' upload-handler']/input").send_keys(videoBean.picPath)
+    sleep(2)
+    browser.find_element_by_xpath("//button[@class='tui2-btn tui2-btn-size-default tui2-btn-primary pgc-button']").click()
+    sleep(5)
+    browser.find_element_by_xpath("//textarea[@class='tui2-input']").send_keys(videoBean.title)
+    browser.find_element_by_xpath("//button[@class='tui2-btn tui2-btn-size-small tui2-btn-primary publish-btn']").click()    
+    print("今日头条小视频上传成功")
 #上传全民小视频
 def uploadQuanming(videoBean):
-    wait = ui.WebDriverWait(browser, 30)  # 10秒内每隔500毫秒扫描1次页面变化
+    wait = ui.WebDriverWait(browser, 100)  # 10秒内每隔500毫秒扫描1次页面变化
     browser.execute_script('window.open()')
     browser.switch_to_window(browser.window_handles[len(browser.window_handles)-1])
     browser.get("https://mcn.baidu.com/publish/upload")
     sleep(3)
-    browser.find_element_by_xpath("//span[@class='ant-upload ant-upload-btn']/input").send_keys(
-        videoBean.videoPath)
+    browser.find_element_by_xpath("//span[@class='ant-upload ant-upload-btn']/input").send_keys(videoBean.videoPath)
     # 等待页面加载
     print("开始上传全民小视频")
     wait.until(lambda driver: browser.find_element_by_xpath("//div[@class='success___1dnqC']"))
     print("全民小视频上传成功")
     browser.find_element_by_xpath("//button[@class='ant-btn ant-btn-primary']").click()  
     sleep(0.3)
-    browser.find_element_by_xpath("//textarea[@class='ant-input']").send_keys(
-        videoBean.title)
+    browser.find_element_by_xpath("//div[@class='video-vertical-cover___3QmJj']/img").click()
+    sleep(3)
+    browser.find_element_by_xpath("//span[@class='ant-upload ant-upload-btn']/input").send_keys(videoBean.picPath)
+    sleep(2)
+    browser.find_element_by_xpath("//textarea[@class='ant-input']").send_keys(videoBean.title)
     browser.find_element_by_xpath("//button[@class='ant-btn btn-publish___3FDWl ant-btn-primary']").click()  
 
 # 上传快手小视频
 def uploadKuaishou(videoBean):
-    wait = ui.WebDriverWait(browser, 30)  # 10秒内每隔500毫秒扫描1次页面变化
+    wait = ui.WebDriverWait(browser, 100)  # 10秒内每隔500毫秒扫描1次页面变化
     browser.execute_script('window.open()')
     browser.switch_to_window(browser.window_handles[len(browser.window_handles)-1])
     browser.get("https://cp.kuaishou.com/article/publish/video")
@@ -61,15 +69,16 @@ def uploadKuaishou(videoBean):
     # 等待页面加载
     print("开始上传快手小视频")
     wait.until(lambda driver: browser.find_element_by_xpath("//i[@class='iconfont icon-check-succeed green']"))
-    print("快手小视频上传成功")
+    browser.find_element_by_xpath("//div[@class='edit-container']//input").send_keys(videoBean.picPath)
+    sleep(2)
     browser.find_element_by_xpath("//textarea[@class='el-textarea__inner']").send_keys(videoBean.title)
-    release=browser.find_element_by_xpath("//div[@class='container__content__body']//button[@class='el-button el-button--primary el-button--medium']")
-    release.click()
+    browser.find_element_by_xpath("//div[@class='container__content__body']//button[@class='el-button el-button--primary el-button--medium']").click()
+    print("快手小视频上传成功")
 
 
 # 上传抖音小视频
 def uploadDouyin(videoBean):
-    wait = ui.WebDriverWait(browser, 30)  # 10秒内每隔500毫秒扫描1次页面变化
+    wait = ui.WebDriverWait(browser, 100)  # 10秒内每隔500毫秒扫描1次页面变化
     browser.execute_script('window.open()')
     browser.switch_to_window(browser.window_handles[len(browser.window_handles)-1])
     browser.get("https://media.douyin.com/#/upload")
@@ -78,6 +87,7 @@ def uploadDouyin(videoBean):
         videoBean.videoPath)
     # 等待页面加载
     print("开始上传抖音小视频")
+
     wait.until(lambda driver: browser.find_element_by_xpath("//div[@class='public-DraftStyleDefault-block public-DraftStyleDefault-ltr']"))
     print("抖音小视频上传成功")
     browser.find_element_by_xpath("//div[@class='notranslate public-DraftEditor-content']").click()
@@ -87,7 +97,7 @@ def uploadDouyin(videoBean):
 
 # 上传大鱼号小视频
 def uploadDayu(videoBean):
-    wait = ui.WebDriverWait(browser, 30)  # 10秒内每隔500毫秒扫描1次页面变化
+    wait = ui.WebDriverWait(browser, 100)  # 10秒内每隔500毫秒扫描1次页面变化
     browser.execute_script('window.open()')
     browser.switch_to_window(browser.window_handles[len(browser.window_handles)-1])
     browser.get("https://mp.dayu.com/dashboard/minivideo/write?spm=a2s0i.db_article_write.content.3.7e293caatzoXXJ")
@@ -96,26 +106,26 @@ def uploadDayu(videoBean):
         videoBean.videoPath)
     # 等待页面加载
     print("开始上传大鱼号小视频")
+    browser.find_element_by_xpath("//div[@class='w-form-field-content']/input[@type='file']").send_keys(videoBean.picPath)
+    browser.find_element_by_xpath("//input[@id='SCOPE_titletext']").send_keys(videoBean.title)
+    sleep(2)
+    browser.find_element_by_xpath("//button[text()='保存']").click()   
     status = browser.find_element_by_xpath("//p[@class='converting_video_name']").text
     while status == "":
         sleep(2)
         status = browser.find_element_by_xpath("//p[@class='converting_video_name']").text
-    sleep(24)
-    print("大鱼号小视频上传成功")
-    browser.find_element_by_xpath("//input[@id='SCOPE_titletext']").send_keys(videoBean.title)
-    cover=browser.find_element_by_xpath("//div[@class='minivideo-cover']")
-    ActionChains(browser).move_to_element(cover).perform()
-    browser.find_element_by_xpath("//div[@class='minivideo-cover']//button[text()='从视频截图中选择']").click()   
-    wait.until(lambda driver: browser.find_element_by_xpath("//div[@class='article-material-image_image-choose']//div"))
-    browser.find_element_by_xpath("//div[@class='article-material-image_image-choose']//div").click()   
-    browser.find_element_by_xpath("//button[text()='下一步']").click()   
     sleep(2)
-    browser.find_element_by_xpath("//button[text()='保存']").click()   
-    sleep(3)
-    # browser.find_element_by_xpath("//div[@class='w-form-field-content']/input[@type='file']").send_keys(videoBean.picPath)
-    # sleep(3)
-    # browser.find_element_by_xpath("//div[@class='widgets-pop w-scrollbar']//button[@class='w-btn w-btn_primary']").click()  
+    print("大鱼号小视频上传成功")
+    # cover=browser.find_element_by_xpath("//div[@class='minivideo-cover']")
+    # ActionChains(browser).move_to_element(cover).perform()
+    # browser.find_element_by_xpath("//div[@class='minivideo-cover']//button[text()='从视频截图中选择']").click()   
+    # wait.until(lambda driver: browser.find_element_by_xpath("//div[@class='article-material-image_image-choose']//div"))
+    # browser.find_element_by_xpath("//div[@class='article-material-image_image-choose']//div").click()   
+    # browser.find_element_by_xpath("//button[text()='下一步']").click()   
     # sleep(2)
+    # browser.find_element_by_xpath("//button[text()='保存']").click()   
+    # sleep(8)
+    
     browser.find_element_by_xpath("//div[@class='minivideo_opt']//button[@class='w-btn w-btn_primary']").click()   
 
 def onPublishVideo(videoBean):
@@ -123,11 +133,11 @@ def onPublishVideo(videoBean):
         print(videoBean.desc)
         print(videoBean.videoPath)
         print(videoBean.picPath)
-        uploadTaoTiao(videoBean)
-        uploadQuanming(videoBean)
+        # uploadTaoTiao(videoBean)
+        # uploadQuanming(videoBean)
         uploadKuaishou(videoBean)
-        uploadDouyin(videoBean)
-        uploadDayu(videoBean)
+        # uploadDouyin(videoBean)
+        # uploadDayu(videoBean)
 
 
 if __name__ == "__main__":
